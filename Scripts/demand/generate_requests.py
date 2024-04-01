88178ea7956700b36ee04d0b6dc547f921ace8df
David Epstein <davideps@tauex.tau.ac.il>
2024-04-01 10:43:25 +0300

    basic demand generation working

--- Scripts/demand/generate_requests.py
+++ Scripts/demand/generate_requests.py
@@ -1,60 +1,63 @@
-# WARNING: Under development!
-# 1) load CSV file where first two columns are origin TAZ (traffic analysis zone) and destination TAZ and remaining columns are time periods (0800_1200, 1200_1400, etc) the integer values in these columns are the number of travellers
-#    from the origin TAZ to the destination TAZ during that time.
-# 2) generates CSV file with time, originx, originy, destx, and desty columns where the origin (x,y) is within the origin TAZ and the destination (x,y) is within the destination TAZ and both points are on the road network.
+# WARNING: Only partially-optimized. Still under development!
+#
+# INPUT: CSV file where first two columns are origin TAZ (traffic analysis zone) and destination TAZ
+#    and remaining columns are time periods (0800_1200, 1200_1400, etc) the integer values in these
+#    columns are the number of travellers from the origin TAZ to the destination TAZ during that time.
+#
+# OUTPUT: CSV file with time, originx, originy, destx, and desty columns where the origin (x,y)
+#    is within the origin TAZ and the destination (x,y) is within the destination TAZ, both points
+#    are on the road network, and the number of points follow a Poisson distribution. 
 #    Can separately add requests to a spatialite database with geoms, depending on the users' workflow.
 
-from spatialite_demand_toolbox import create_sqlite_cursor, create_point_xy_taz
+from spatialite_demand_toolbox import create_sqlite_cursor, create_points_table, create_points_on_streets_in_OD, insert_point_in_table
 import pandas as pd
 import numpy as np
 import random
 import time
 
-period = '8_10' # currently handles one period of the data at a time
-scale_prc = 10
+period = '8_10' # currently handles one period of demand at a time
+scale_prc = 5  # scale demand by this percent
 basedir = "D:/Documents/AUTOlab/Jerusalem/SUM-Optimization"
 db_filepath = f"{basedir}/JerusalemData/gis/jerusalem.sqlite"
 demand_filepath = f"{basedir}/JerusalemData/demand/matrix_15destinations.csv"
 demand_df = pd.read_csv(demand_filepath)
+ignore = ["4711"] # ignore these taz (outside target neighborhood or city, measurement error, etc)
 
 requests = []
 start_hour = int(period.split("_")[0])
 end_hour = int(period.split("_")[1])
 num_hours = end_hour - start_hour
 np.random.seed()
+
+# create new table for demand instance
+tablename = f"demand_{scale_prc}prc"
 conn, cursor = create_sqlite_cursor(db_filepath)
+create_points_table(cursor,tablename)
 
+request_id = 0
 print(f"GENERATING REQUESTS FROM TIME {start_hour} TO {end_hour}")
-for index, row in demand_df.iterrows():
-    n = 1
-    origin_taz_num = int(row['origin'])
-    dest_taz_num = int(row['destination'])
-    requests_in_period = row[period]
-    scaled_requests_in_period = requests_in_period * scale_prc * .01
-    print(f"row: {index}, origin: {origin_taz_num}, destination: {dest_taz_num}, volume: {requests_in_period} scaled to {scaled_requests_in_period}")
-    poisson = np.random.poisson(scaled_requests_in_period)
-    for _ in range(poisson):
-        request_time = np.random.uniform(start_hour,end_hour) 
-        origin_x, origin_y, origin_geom = create_point_xy_taz(cursor,origin_taz_num)
-        dest_x, dest_y, dest_geom = create_point_xy_taz(cursor,dest_taz_num)
-        # save origin and destination as 
-
-
-        # for writing to CSV later
-        requests.append([request_time,origin_taz_num,dest_taz_num,origin_x,origin_y,dest_x,dest_y])
-        n+=1
+for demand_row_id, row in demand_df.iterrows():
+    origin_taz_num = f"{int(row['origin'])}".zfill(4)
+    dest_taz_num = f"{int(row['destination'])}".zfill(4)
+    if origin_taz_num not in ignore and dest_taz_num not in ignore:
+        requests_in_period = row[period]
+        scaled_requests_in_period = requests_in_period * scale_prc * .01
+        print(f"row: {demand_row_id}, origin: {origin_taz_num}, destination: {dest_taz_num}, volume: {requests_in_period} scaled to {scaled_requests_in_period}")
+        num_requests = np.random.poisson(scaled_requests_in_period)
+        if num_requests > 0:
+            ods = create_points_on_streets_in_OD(cursor, origin_taz_num, dest_taz_num, num_requests)
+            for od in ods:
+                origin_x, origin_y, origin_geom, dest_x, dest_y, dest_geom = od
+                request_time = np.random.uniform(start_hour,end_hour) 
+                # save origin and destination to sqlite db
+                insert_point_in_table(cursor,tablename,request_id,demand_row_id,request_time,"origin",origin_taz_num, origin_x,origin_y,origin_geom)
+                insert_point_in_table(cursor,tablename,request_id,demand_row_id,request_time,"dest",dest_taz_num, dest_x,dest_y,dest_geom)
+                # for writing to CSV later
+                requests.append([request_id,demand_row_id,request_time,origin_taz_num,dest_taz_num,origin_x,origin_y,dest_x,dest_y])
+                request_id+=1
+conn.commit()
 
 # write to file
-
-columns = ['request_time', 'origin_taz', 'dest_taz', 'origin_x', 'origin_y', 'dest_x', 'dest_y']
-
-# Create a DataFrame and save as CSV
+columns = ['request_id','demand_row_id','request_time', 'origin_taz', 'dest_taz', 'origin_x', 'origin_y', 'dest_x', 'dest_y']
 df = pd.DataFrame(requests, columns=columns)
 df.to_csv(f"{basedir}/demand/demand_{scale_prc}prc.csv", index=False) 
-
-
-
-
-
-
-
--- Scripts/demand/spatialite_demand_toolbox.py
+++ Scripts/demand/spatialite_demand_toolbox.py
@@ -61,41 +61,42 @@ def create_point_on_street(cursor,street_geom,taz_geom):
     return point_geom
 
 
-def drop_points_table(conn,cursor,table_name):
-    query = f"DROP TABLE IF EXISTS {table_name};"
-    cursor.execute(query)
-    # Probably not needed:
-    #cursor.execute("DELETE FROM geometry_columns WHERE f_table_name = 'taz_points';")
-    #cursor.execute("SELECT DisableSpatialIndex('taz_points', 'geometry');")  # Disable the spatial index
-    #cursor.execute("DROP TABLE IF EXISTS idx_taz_points_geometry;")  # Drop the index table if it was created\
-    conn.commit()
-
-
-def create_points_table(cursor,table_name):
-
+def create_points_table(cursor,tablename): 
     # Visualizing trips with traditional GIS tools is sometimes easier if origin and destination are
     # stored as separate rows (not a single row with two spatial geoms). "part" is either origin
     # or destination with a shared "id".
-
-    query = f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER, request_time FLOAT, part TEXT, x DOUBLE, y DOUBLE, UNIQUE(id, part));"
+    query = f"DROP TABLE IF EXISTS {tablename};"
     cursor.execute(query)
-    query = f"SELECT AddGeometryColumn({table_name}, 'geometry', 2039, 'POINT', 'XY');"
+    query = f"CREATE TABLE IF NOT EXISTS {tablename} (id INTEGER, demand_row, request_time FLOAT, part TEXT, taz, x DOUBLE, y DOUBLE, UNIQUE(id, part));"
+    cursor.execute(query)
+    query = f"SELECT AddGeometryColumn('{tablename}', 'geometry', 2039, 'POINT', 'XY');"
     try:
         cursor.execute(query)
     except sqlite3.OperationalError as e:
         print(f"AddGeometryColumn() skipped: {e}")
     try:
-        query = f"SELECT CreateSpatialIndex({table_name}, 'geometry');"
+        query = f"SELECT CreateSpatialIndex('{tablename}', 'geometry');"
         cursor.execute(query)
     except sqlite3.OperationalError as e:
         print(f"CreateSpatialIndex() skipped: {e}")
 
-def create_point_xy_taz(cursor, taz_num):
-    taz_geom = get_selected_taz(cursor,taz_num)
-    streets = get_streets_in_taz(cursor,taz_geom)    
-    points = []
+
+def create_points_on_streets_in_OD(cursor, origin_taz_num, dest_taz_num, num_requests):
+    result = []
+    orig_taz_geom = get_selected_taz(cursor,origin_taz_num)
+    dest_taz_geom = get_selected_taz(cursor,dest_taz_num)
+    origin_street_geoms = get_streets_in_taz(cursor,orig_taz_geom)
+    dest_street_geoms = get_streets_in_taz(cursor,dest_taz_geom)
+    for _ in range(num_requests):
+        origin_x, origin_y, origin_geom = create_point_on_street_in_taz(cursor, origin_taz_num, orig_taz_geom, origin_street_geoms)
+        dest_x, dest_y, dest_geom = create_point_on_street_in_taz(cursor, dest_taz_num, dest_taz_geom, dest_street_geoms)
+        result.append([origin_x, origin_y, origin_geom, dest_x, dest_y, dest_geom])
+    return result
+
+
+def create_point_on_street_in_taz(cursor, taz_num, taz_geom, street_geoms):
     # Randomly select street
-    street_id, street_geom = random.choice(streets)
+    street_id, street_geom = random.choice(street_geoms)
     point_geom = create_point_on_street(cursor,street_geom,taz_geom)
     if point_geom:
         # Extract X and Y from the point geometry
@@ -107,48 +108,7 @@ def create_point_xy_taz(cursor, taz_num):
     else:
         raise Exception(f"Failed to create point in TAZ {taz_num} on street {street_geom}")
 
-def insert_point_in_table(request_time,part,x,y,geom):
-    pass
-
-    
-    # # Insert the point along with X and Y
-    #     cursor.execute("""
-    #         INSERT INTO taz_points (X, Y, geometry) VALUES (?, ?, ST_GeomFromText(?,2039)) 
-    #     """, (x, y, point_geom)) # if SRID e.g. 2039 is not specified here it defaults to zero and causes problems
-
-
-
-
-def create_point_within_taz(db_path, taz_num, num_points=1):
-    conn, cursor = create_sqlite_cursor(db_path)
-    taz_geom = get_selected_taz(cursor,taz_num)
-    streets = get_streets_in_taz(cursor,taz_geom)    
-    points = []
-    while len(points) < num_points:
-        # Randomly select street
-        street_id, street_geom = random.choice(streets)
-        point_geom = create_point_on_street(cursor,street_geom,taz_geom)
-        if point_geom:
-            points.append(point_geom)
-    drop_points_table(conn,cursor,"taz_points")
-    create_points_table(cursor,"taz_points")
-
-    # Insert the generated points into the new table
-    for point_geom in points:
-    # Extract X and Y from the point geometry
-        cursor.execute("""
-            SELECT ST_X(ST_GeomFromText(?,2039)), ST_Y(ST_GeomFromText(?,2039))
-        """, (point_geom, point_geom))
-        x, y = cursor.fetchone()
-    
-    # Insert the point along with X and Y
-        cursor.execute("""
-            INSERT INTO taz_points (X, Y, geometry) VALUES (?, ?, ST_GeomFromText(?,2039)) 
-        """, (x, y, point_geom)) # if SRID e.g. 2039 is not specified here it defaults to zero and causes problems
-
-    conn.commit()
-    conn.close()
-
-# MAIN
-db_path = 'D:/Documents/AUTOlab/Jerusalem/GIS/jerusalem.sqlite'
-create_points_within_taz(db_path, 4604, 100)
+def insert_point_in_table(cursor,tablename,id,demand_row,request_time,part,taz, x,y,point_geom): # potential security issue since tablename is parameter
+    print(f"{id} {request_time} {part} {x} {y} {point_geom}")
+    query = f"""INSERT INTO {tablename} (id, demand_row, request_time, part, taz, X, Y, geometry) VALUES (?, ?, ?, ?, ?, ?, ?, ST_GeomFromText(?,2039))""" # if SRID e.g. 2039 is not specified here it defaults to zero and causes problems
+    cursor.execute(query,(id,demand_row,request_time,part,taz,x,y,point_geom))
